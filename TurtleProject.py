d24a01297ac6480bab3db70dc149c970dad4aefe
Leah C <101465290+Leah9@users.noreply.github.com>
2022-05-09 16:23:17 +0100

    End of day one, Turtle nice shapes.

---
+++ TurtleProject.py
@@ -0,0 +1,37 @@
+from turtle import *
+import random
+
+colormode(255)
+reset()
+def square(size):
+    forward(size)
+    right(90)
+    forward(size)
+    right(90)
+    forward(size)
+    right(90)
+    forward(size)
+    right(90)
+
+def offsetDownRight(offset):
+    penup()
+    forward(offset)
+    right(90)
+    forward(offset)
+    left(90)
+    pendown()
+
+
+for i in range(18):
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    fillcolor(r,g,b)
+    begin_fill()
+    square(100)
+    end_fill()
+    right(20)
+    #offsetDownRight(10)
+
+input("Finished, press enter to close")
+
